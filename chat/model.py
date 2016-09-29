8f2efed04d3d046847e0b9db2a95e472a2540d1d
Daniel Milde <daniel.milde@firma.seznam.cz>
2016-09-29 14:52:06 +0200

    user jako trida, magicke metody

--- chat/model.py
+++ chat/model.py
@@ -1,4 +1,11 @@
-from collections import namedtuple
 
+class User:
+    def __init__(self, ip, name):
+        self.ip = ip
+        self.name = name
 
-User = namedtuple('User', ['ip', 'name'])
+    def __str__(self):
+        return 'User {}'.format(self.name)
+
+    def __repr__(self):
+        return '<User ip={s.ip} name={s.name}>'.format(s=self)
