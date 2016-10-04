ed3ee245285a3f2b46f743bc8c79accd00402297
Daniel Milde <daniel.milde@firma.seznam.cz>
2016-10-04 14:27:43 +0200

    ukladani jmena do souboru, konfigurace

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+from configparser import ConfigParser
+
 from dialog import Dialog
 
+CONFIG_FILE = 'chat.ini'
+DEFAULT_CONFIG = {'user': {'name': ''}}
 
-def get_name():
+
+def main():
+    config = get_config()
+    name = get_name_with_config(config)
+    save_config(config)
+
+    print(name)
+
+
+def get_config():
+    config = ConfigParser()
+    config.read_dict(DEFAULT_CONFIG)
+    config.read(CONFIG_FILE)
+    return config
+
+
+def save_config(config):
+    with open(CONFIG_FILE, 'w') as configfile:
+        config.write(configfile)
+
+
+def get_name_with_config(config):
+    default = config['user']['name']
+    name = get_name(default)
+    config['user']['name'] = name
+    return name
+
+
+def get_name(default=''):
     dialog = Dialog()
-
     while True:
-        code, name = dialog.inputbox('Napis jmeno')
+        code, name = dialog.inputbox('Napis jmeno', init=default)
         if code == dialog.OK and name:
             break
     return name
 
 
-print(get_name())
+main()
